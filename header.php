ca54a8fd7acbebd0a65cd3e9fc7d03cc666fc3ba
Jsheen98 <jsheendev@gmail.com>
2023-07-14 16:05:19 -0600

    Created a header and footer for website

---
+++ header.php
@@ -0,0 +1,42 @@
+<style>
+    * {
+        padding: 0;
+        margin: 0;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    body {
+        background-color: #f7f7eb;
+        font-family: tahoma;
+    }
+
+    header div {
+        padding: 20px;
+    }
+
+    header a {
+        color: white;
+    }
+
+    header {
+        background-color: #dd7f5d;
+        display: flex;
+        color: white;
+        justify-content: center;
+        align-items: center;
+    }
+    footer{
+        padding: 20px;
+        text-align: center;
+        background-color: #eee;
+    }
+</style>
+<header>
+    <div><a href="home.php">Home</a></div>
+    <div><a href="profile.php">Profile</a></div>
+    <div><a href="login.php">Login</a></div>
+    <div><a href="signup.php">Signup</a></div>
+</header>
